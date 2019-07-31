657c2af00ea9b97993f1c32e7bc913318b946a0f
Lee Hudson <lee.hudson@diamond.ac.uk>
2019-07-31 13:48:48 +0100

    Dunno

--- r7AravisBuilder/startIOC01.sh
+++ r7AravisBuilder/startIOC01.sh
@@ -1,4 +1,4 @@
-kill -9 `pidof ./BL99I-LCH-IOC-01 stBL99I-LCH-IOC-01.boot`
-cd /home/jjc62351/work/Builder/r7AravisBuilder/iocs/BL99I-LCH-IOC-01/bin/linux-x86_64
-./stBL99I-LCH-IOC-01-gui &
-./stBL99I-LCH-IOC-01.sh 
+kill -9 `pidof ./BLTEST-LCH-IOC-01 stBLTEST-LCH-IOC-01.boot`
+cd /home/jjc62351/work/builderTestBench/r7AravisBuilder/iocs/BLTEST-LCH-IOC-01/bin/linux-x86_64
+./stBLTEST-LCH-IOC-01-gui &
+./stBLTEST-LCH-IOC-01.sh 
