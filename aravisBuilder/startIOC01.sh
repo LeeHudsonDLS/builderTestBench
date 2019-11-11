f2d7d29121a4a1222adfd110533c998cc87abf1f
Lee Hudson <lee.hudson@diamond.ac.uk>
2019-11-11 09:14:13 +0000

    Something something commit

--- aravisBuilder/startIOC01.sh
+++ aravisBuilder/startIOC01.sh
@@ -1,6 +1,6 @@
 kill -9 `pidof ./BL99I-LCH-IOC-01 stBL99I-LCH-IOC-01.boot`
-cd /dls_sw/work/R3.14.12.3/support/asyn/opi/edm
-edm -m "P=BL99I-LCH-ASYN-,R=01" asynRecord.edl&
-cd /home/jjc62351/work/Builder/aravisBuilder/iocs/BL99I-LCH-IOC-01/bin/linux-x86_64
+#cd /dls_sw/work/R3.14.12.3/support/asyn/opi/edm
+#edm -m "P=BL99I-LCH-ASYN-,R=01" asynRecord.edl&
+cd /home/jjc62351/work/builderTestBench/aravisBuilder/iocs/BL99I-LCH-IOC-01/bin/linux-x86_64
 ./stBL99I-LCH-IOC-01-gui &
 ./stBL99I-LCH-IOC-01.sh 
---
+++ r7ADAravisBuilder/startIOC01.sh
@@ -0,0 +1,4 @@
+kill -9 `pidof ./BLTEST-LCH-IOC-01 stBLTEST-LCH-IOC-01.boot`
+cd /home/jjc62351/work/builderTestBench/r7ADAravisBuilder/iocs/BLTEST-LCH-IOC-01/bin/linux-x86_64
+./stBLTEST-LCH-IOC-01-gui &
+./stBLTEST-LCH-IOC-01.sh 
