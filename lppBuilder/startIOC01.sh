6476f7f4acbeb9f306d0e8ff81939d8f57171710
Lee Hudson <lee.hudson@diamond.ac.uk>
2021-02-10 11:46:45 +0000

    Update

--- lppBuilder/startIOC01.sh
+++ lppBuilder/startIOC01.sh
@@ -1,4 +1,4 @@
 kill -9 `pidof ./BL99I-LCH-IOC-01 stBL99I-LCH-IOC-01.boot`
-cd /home/jjc62351/work/Builder/leyboldBuilder/iocs/BL99I-LCH-IOC-01/bin/linux-x86_64
+cd /home/jjc62351/work/builderTestBench/lppBuilder/iocs/BL99I-LCH-IOC-01/bin/linux-x86_64
 ./stBL99I-LCH-IOC-01-gui &
 ./stBL99I-LCH-IOC-01.sh 
